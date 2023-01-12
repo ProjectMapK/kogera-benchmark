925e697ace367216a77a51791ebd2c099fe6a7ea
wrongwrong <boranti1995@gmail.com>
2023-01-13 00:14:50 +0900

    base

---
+++ src/jmh/kotlin/org/wrongwrong/ser/1Props.kt
@@ -0,0 +1,23 @@
+package org.wrongwrong.ser
+
+import org.openjdk.jmh.annotations.Benchmark
+import org.openjdk.jmh.annotations.Level
+import org.openjdk.jmh.annotations.Scope
+import org.openjdk.jmh.annotations.Setup
+import org.openjdk.jmh.annotations.State
+import org.wrongwrong.fromRandomInts
+import org.wrongwrong.mapper
+
+@State(Scope.Benchmark)
+open class A_OneProps {
+    data class Dst(val p0: Int)
+    lateinit var target: Dst
+
+    @Setup(Level.Trial)
+    fun setUp() {
+        target = ::Dst.fromRandomInts()
+    }
+
+    @Benchmark
+    fun one() = mapper.writeValueAsString(target)
+}
---
+++ src/jmh/kotlin/org/wrongwrong/ser/5Props.kt
@@ -0,0 +1,29 @@
+package org.wrongwrong.ser
+
+import org.openjdk.jmh.annotations.Benchmark
+import org.openjdk.jmh.annotations.Level
+import org.openjdk.jmh.annotations.Scope
+import org.openjdk.jmh.annotations.Setup
+import org.openjdk.jmh.annotations.State
+import org.wrongwrong.fromRandomInts
+import org.wrongwrong.mapper
+
+@State(Scope.Benchmark)
+open class E_FiveProps {
+    data class Dst(
+        val p0: Int,
+        val p1: Int,
+        val p2: Int,
+        val p3: Int,
+        val p4: Int
+    )
+    lateinit var target: Dst
+
+    @Setup(Level.Trial)
+    fun setUp() {
+        target = ::Dst.fromRandomInts()
+    }
+
+    @Benchmark
+    fun five() = mapper.writeValueAsString(target)
+}
