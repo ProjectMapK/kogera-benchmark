6d0b737fe3951a38e746c412a7f7966e53aed347
wrongwrong <boranti1995@gmail.com>
2023-10-09 02:48:33 +0900

    Update kogera version

--- build.gradle.kts
+++ build.gradle.kts
@@ -51,7 +51,7 @@ val isSingleShot: Boolean = getOptionOrDefault("isSingleShot", false)
 val isCi: Boolean = System.getenv().containsKey("CI") // True when executed in GitHub Actions
 val ciFileName: String? = project.properties["fileName"] as String?
 
-val kogeraVersion = "7dc2a10567"
+val kogeraVersion = "2.15.2-beta5"
 val originalVersion = "2.15.2"
 
 dependencies {
