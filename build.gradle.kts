62148edb0e75ddc0f486429e12cd2d70fe4240e3
wrongwrong <k163377@users.noreply.github.com>
2024-01-21 21:50:05 +0900

    Update jackson versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -51,8 +51,8 @@ val isSingleShot: Boolean = getOptionOrDefault("isSingleShot", false)
 val isCi: Boolean = System.getenv().containsKey("CI") // True when executed in GitHub Actions
 val ciFileName: String? = project.properties["fileName"] as String?
 
-val kogeraVersion = "2.16.0-beta9"
-val originalVersion = "2.16.0"
+val kogeraVersion = "2.16.1-beta10"
+val originalVersion = "2.16.1"
 
 dependencies {
     jmhImplementation(kotlin("reflect"))
