d86aeef08126108a402cb8479bd5839246a4542f
wrongwrong <boranti1995@gmail.com>
2025-03-22 21:11:09 +0900

    Corrected to explicit specification due to change in option defaults
    wrt https://github.com/ProjectMapK/jackson-module-kogera/pull/290

--- src/jmh/kotlin/org/wrongwrong/Mapper.kt
+++ src/jmh/kotlin/org/wrongwrong/Mapper.kt
@@ -3,11 +3,8 @@ package org.wrongwrong
 import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.KotlinFeature
-import com.fasterxml.jackson.module.kotlin.KotlinModule
-import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper as originalMapper
 import io.github.projectmapk.jackson.module.kogera.KotlinFeature as KogeraFeature
-import io.github.projectmapk.jackson.module.kogera.KotlinModule as KogeraModule
 import io.github.projectmapk.jackson.module.kogera.jacksonObjectMapper as kogeraMapper
 
 private fun ObjectMapper.applyCommonOptions() =
@@ -17,23 +14,28 @@ enum class Mapper {
     Original {
         override val value: ObjectMapper = originalMapper {
             enable(KotlinFeature.KotlinPropertyNameAsImplicitName)
+            disable(KotlinFeature.SingletonSupport)
+            disable(KotlinFeature.StrictNullChecks)
         }.applyCommonOptions()
     },
     Kogera {
-        override val value: ObjectMapper = kogeraMapper().applyCommonOptions()
+        override val value: ObjectMapper = kogeraMapper {
+            disable(KogeraFeature.SingletonSupport)
+            disable(KogeraFeature.StrictNullChecks)
+        }.applyCommonOptions()
     },
     OriginalStrictNullCheck {
-        override val value: ObjectMapper = ObjectMapper()
-            .registerKotlinModule {
-                enable(KotlinFeature.StrictNullChecks)
-                    .enable(KotlinFeature.KotlinPropertyNameAsImplicitName)
-            }
-            .applyCommonOptions()
+        override val value: ObjectMapper = originalMapper {
+            enable(KotlinFeature.KotlinPropertyNameAsImplicitName)
+            enable(KotlinFeature.StrictNullChecks)
+            disable(KotlinFeature.StrictNullChecks)
+        }.applyCommonOptions()
     },
     KogeraStrictNullCheck {
-        override val value: ObjectMapper = ObjectMapper()
-            .registerModule(KogeraModule.Builder().enable(KogeraFeature.StrictNullChecks).build())
-            .applyCommonOptions()
+        override val value: ObjectMapper = kogeraMapper {
+            enable(KogeraFeature.StrictNullChecks)
+            disable(KogeraFeature.SingletonSupport)
+        }.applyCommonOptions()
     };
 
     abstract val value: ObjectMapper
